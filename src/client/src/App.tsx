f279ac7b9be6e1ea7832c801077ae8a091547b5a
Niko Draca <me@nikodraca.com>
2020-10-24 13:08:09 -0400

    Add client env vars

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
 function App() {
+  const {REACT_APP_SLACK_CLIENT_ID, REACT_APP_SLACK_REDIRECT_URI} = process.env
+  const slackAuthUrl = `https://slack.com/oauth/authorize?client_id=${REACT_APP_SLACK_CLIENT_ID}&scope=users.profile:write&redirect_uri=${REACT_APP_SLACK_REDIRECT_URI}`
+
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://slack.com/oauth/authorize?client_id=1182059129521.1447672242117&scope=users.profile:write&redirect_uri=http://localhost:3000">Authorize with Slack</a>
+        <a href={slackAuthUrl}>Authorize with Slack</a>
       </header>
     </div>
   );
 }
 
 export default App;
-
-
-// https://slack.com/oauth/authorize?client_id=1182059129521.1447672242117&scope=users.profile:write&redirect_uri=http://localhost:3000
-
-// http://localhost:3000/?code=1182059129521.1456927755124.37be7d8e32643d34c429b282eff00481586bac01abc03ef9599c55357793ced1&state=
